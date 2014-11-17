52634f2c8a7b6a00da2a98b460237dd2d563f8cb
Matt Lo <mattlo.developer@gmail.com>
2014-11-17 03:26:49 -0600

    abstracted out button listener service, created button listener directive

--- button-test/button-test.js
+++ button-test/button-test.js
@@ -1,75 +1,12 @@
-var app = angular.module('buttonTest', []);
-
-// quick debug on XB1
-window.onerror = console.log = function () {
-	alert(JSON.stringify(arguments));
-}
-
-app.run(function ($gamepad, $rootScope) {
-	$rootScope.details = "abc";
-
-	$gamepad.on('button.A_BUTTON.down', function (e, button) {
-		$rootScope.details = new Date
-	});
-});
-
-app.factory('$gamepad', function ($window, $rootScope) {
-
-	var $gamepad,
-		$scope = $rootScope.$new();
-
-	$gamepad = {
-		// button constants
-		action: {
-			A_BUTTON: 0,
-			B_BUTTON: 1,
-			X_BUTTON: 2,
-			Y_BUTTON: 3,
-			LEFT_SHOULDER: 4,
-			RIGHT_SHOULDER: 5,
-			UP_DPAD: 12,
-			DOWN_DPAD: 13,
-			LEFT_DPAD: 14,
-			RIGHT_DPAD: 15,
-			VIEW_BUTTON: 8,
-			MENU_BUTTON: 9
-		},
-		instance: function () {
-			return $window.navigator.getGamepads()[0]
-		},
-		on: function (event, callback) {
-			return $scope.$on.call($scope, event, function (arguments) {
-				callback.apply(this, arguments);
-				$rootScope.$apply();
+app.directive('gameButtonListener', function ($rootScope, $gamepad) {
+	return {
+		scope: true,
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			$gamepad.on('button.' + attrs.gameButtonListener + '.down', function () {
+				scope.button = arguments;
+				scope.button.timestamp = new Date;
 			});
 		}
-	};
-
-	function eventLoop() {
-		var button,
-			gamepadRef = $gamepad.instance();
-
-		if (typeof gamepadRef !== 'undefined') {
-			// listen for all buttons
-			for (button in $gamepad.action) {
-				if (gamepadRef.buttons[$gamepad.action[button]].pressed) {
-					// dispatch event
-					$scope.$broadcast(
-						'button.' + button + '.down', 
-						gamepadRef.buttons[button], 
-						$gamepad.action[button]
-						);
-				}
-			}
-		}
-
-		// run forever!
-		$window.requestAnimationFrame(eventLoop)
 	}
-
-	// run loop
-	eventLoop();
-
-	// return controller singleton
-	return $gamepad;
 });
---
+++ common/common.js
@@ -0,0 +1,67 @@
+var app = angular.module('buttonTest', []);
+
+// quick debug on XB1
+window.onerror = console.log = function () {
+	alert(JSON.stringify(arguments));
+}
+
+app.factory('$gamepad', function ($window, $rootScope) {
+
+	var $gamepad,
+		$scope = $rootScope.$new();
+
+	$gamepad = {
+		// button constants
+		action: {
+			A_BUTTON: 0,
+			B_BUTTON: 1,
+			X_BUTTON: 2,
+			Y_BUTTON: 3,
+			LEFT_SHOULDER: 4,
+			RIGHT_SHOULDER: 5,
+			UP_DPAD: 12,
+			DOWN_DPAD: 13,
+			LEFT_DPAD: 14,
+			RIGHT_DPAD: 15,
+			VIEW_BUTTON: 8,
+			MENU_BUTTON: 9
+		},
+		instance: function () {
+			return $window.navigator.getGamepads()[0]
+		},
+		on: function (event, callback) {
+			return $scope.$on.call($scope, event, function () {
+				callback.apply(null, arguments);
+				$rootScope.$apply();
+			});
+		}
+	};
+
+	function eventLoop() {
+		var button,
+			gamepadRef = $gamepad.instance();
+
+		if (typeof gamepadRef !== 'undefined') {
+			// listen for all buttons
+			for (button in $gamepad.action) {
+				if (gamepadRef.buttons[$gamepad.action[button]].pressed) {
+					// dispatch event
+					$scope.$broadcast(
+						'button.' + button + '.down', 
+						gamepadRef.buttons[$gamepad.action[button]].value, 
+						$gamepad.action[button]
+					);
+				}
+			}
+		}
+
+		// run forever!
+		$window.requestAnimationFrame(eventLoop)
+	}
+
+	// run loop
+	eventLoop();
+
+	// return controller singleton
+	return $gamepad;
+});
